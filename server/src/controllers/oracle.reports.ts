5eac9e4305dbca2134e29fc343bc32c2d5abcc86
llOrtegall <IvanOrtega_97@hotmail.com>
2024-11-29 09:55:09 -0500

    validar query para pasar fechas

--- server/src/controllers/oracle.reports.ts
+++ server/src/controllers/oracle.reports.ts
@@ -2,9 +2,20 @@ import { connectionOracle } from '../connection/oracledb';
 import { Request, Response } from 'express';
 import { Connection } from 'oracledb';
 
-const FunBetweenDates = (startDate: string, endDate: string) => {
-`BETWEEN TO_DATE('${startDate}', 'DD/MM/YYYY') AND TO_DATE('${endDate}', 'DD/MM/YYYY')`;
-}
+export type RowType = [
+  string, // FECHAPAGO
+  string, // SERIE
+  number, // PREMIO
+  string, // VENDEDOR
+  string, // NOMBRES
+  string, // HORA
+  number, // PUNTO_VTA_PAGO
+  number, // APLICACION
+  string // MUNICIPIO
+];
+
+const FunBetweenDates = (startDate: string, endDate: string) => `fechapago BETWEEN TO_DATE('${startDate}', 'DD/MM/YYYY') AND TO_DATE('${endDate}', 'DD/MM/YYYY')`;
+const aplanarString = (arr: number[]) => arr.map((el) => `'${el}'`).join(',');
 
 export const getReportOracle = async (req: Request, res: Response) => {
   /*const data = req.body;
@@ -25,7 +36,12 @@ export const getReportOracle = async (req: Request, res: Response) => {
   connection = await pool.getConnection();
 
   try {
-    const result = await connection.execute(
+    const { rows } = await connection.execute<number[]>('SELECT distinct usuadocu FROM cerberus.ms_usuario WHERE grupcodi in (16,17)')
+    // unir los arrays de arrays en un solo array
+    const arrayUsers = rows?.flat();
+    const strUsers = aplanarString(arrayUsers!);
+
+    const { rows: rows2, metaData } = await connection.execute<RowType[]>(
       `
       SELECT 
         TT.FECHAPAGO, TT.SERIE, TT.PREMIO, TT.VENDEDOR, UPPER(PE.NOMBRES||' '||PE.APELLIDO1||' '||PE.APELLIDO2) NOMBRES,
@@ -33,17 +49,17 @@ export const getReportOracle = async (req: Request, res: Response) => {
       FROM(
         SELECT FECHAPAGO, SERIE||NUMERO SERIE, TOTALPREMIO-RETEFUENTE PREMIO, SUBSTR(LOGINCAJERO,4) VENDEDOR, HORA, PUNTO_VTA_PAGO, 1 APLICACION
         FROM premiospersonaproveedor
-        WHERE fechapago BETWEEN TO_DATE('01/10/2024', 'DD/MM/YYYY') AND TO_DATE('31/10/2024', 'DD/MM/YYYY')
-        AND documentocajero IN (SELECT distinct usuadocu FROM cerberus.ms_usuario WHERE grupcodi in (16,17))
+        WHERE ${FunBetweenDates('01/10/2024', '31/10/2024')}
+        AND documentocajero IN (${strUsers})
       UNION ALL
         SELECT FECHAPAGO, SERIE||NUMERO, TOTALPREMIO-RETEFUENTE PREMIO, SUBSTR(LOGINCAJERO,4) VENDEDOR, HORA, PUNTO_VTA_PAGO, 2 APLICACION
         FROM premiospersonaproveedor@CONSULTAS 
-        WHERE fechapago BETWEEN TO_DATE('01/10/2024', 'DD/MM/YYYY') AND TO_DATE('31/10/2024', 'DD/MM/YYYY')
-        AND documentocajero in (SELECT distinct usuadocu FROM cerberus.ms_usuario WHERE grupcodi in (16,17))
+        WHERE ${FunBetweenDates('01/10/2024', '31/10/2024')}
+        AND documentocajero in (${strUsers})
         AND serie||numero NOT IN (
           SELECT distinct SERIE||NUMERO FROM premiospersonaproveedor 
-          WHERE fechapago BETWEEN TO_DATE('01/10/2024', 'DD/MM/YYYY') AND TO_DATE('31/10/2024', 'DD/MM/YYYY')
-          AND documentocajero IN (SELECT distinct usuadocu FROM cerberus.ms_usuario WHERE grupcodi in (16,17))
+          WHERE ${FunBetweenDates('01/10/2024', '31/10/2024')}
+          AND documentocajero IN (${strUsers})
           )
         )
       TT, PERSONAS PE, UBICACIONNEGOCIOS UN
@@ -54,9 +70,25 @@ export const getReportOracle = async (req: Request, res: Response) => {
       `
     );
 
-    res.status(200).json(result);
+    const data = rows2?.map(row => {
+      return metaData?.reduce((acc, meta, index) => {
+        acc[meta.name.toLowerCase()] = row[index];
+        return acc;
+      }, {} as Record<string | number, any>);
+    });
+
+
+    res.status(200).json(data);
   } catch (error) {
     console.log(error)
     res.status(500).json(error);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (error) {
+        console.log('Error al cerrar la conexión', error);
+      }
+    }
   }
 }
