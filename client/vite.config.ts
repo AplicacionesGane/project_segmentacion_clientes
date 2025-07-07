01662f2078ab384a47caa491c0f63520be27f079
llOrtegall <IvanOrtega_97@hotmail.com>
2025-07-07 16:27:00 -0500

    shadcn installed addd firts component

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,16 @@
 import react from '@vitejs/plugin-react-swc';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite';
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true
-  },
-  plugins: [
-    react(),
-    tailwindcss()
+  server: { host: true },
+  plugins: [react(), tailwindcss()
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
