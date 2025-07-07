1d32522d2623ad58aa9453fc39e3104b80bae6a8
llOrtegall <IvanOrtega_97@hotmail.com>
2025-07-07 16:16:28 -0500

    update works but reompio todo

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 })
